6eda01d4ed767c7325527e9356f02898ab3df73f
plezcano <plezcanovilchez@hotmail.com>
2026-04-21 19:49:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ============================================
 // ESTILOS
